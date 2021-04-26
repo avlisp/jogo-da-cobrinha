6030f16492e7b5bdfb9977d0f6663b2c51dd99f5
Anna sganderla <sganderlakarlaanna@gmail.com>
2021-04-26 15:20:50 -0300

    criando cobrinha

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); // context renderiza o desenho
 let box = 32;
 
+// variavel para cobra andar
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
 // funçao pra desenhar e definir o canvas
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -11,5 +18,16 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+// funçao para criar a cobra 
+function criarCobrinha(){
+    for(i = 0; i < snake.length; i++) {
+        context.fillStyle = "green"
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+
 // chamando a funçao
 criarBG();
+criarCobrinha();
+

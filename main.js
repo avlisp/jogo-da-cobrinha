936be0d28ae9ddc5f5b4c4493797379caca1eae6
Anna sganderla <sganderlakarlaanna@gmail.com>
2021-04-26 15:38:57 -0300

    adicionando movimento a cobrinha

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ snake[0] = {
     y: 8 * box
 }
 
+// variavel para direçao da criarCobrinha
+let direction = "right";
+
 // funçao pra desenhar e definir o canvas
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -26,8 +29,37 @@ function criarCobrinha(){
     }
 }
 
+function iniciarJogo() {
+    // chamando a funçao
+    criarBG();
+    criarCobrinha();
 
-// chamando a funçao
-criarBG();
-criarCobrinha();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // coordenadas onde a cobrinha vai seguir
+    if (direction == "right") snakeX += box;    // se a direçao for igual a right adicione 1 a box (quadradinho) na posiçao x
+    if (direction == "left") snakeX -= box;     // se a direçao for igual a left subtraia 1 a box (quadradinho) na posiçao x
+    if (direction == "up") snakeY += box;
+    if (direction == "down") snakeY -= box;
+
+    // funçao que retira um elemento do array
+    snake.pop();
+ 
+    // variável que adiciona um nova cabeça
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+
+
+// variavel para iniciar jogo // 100 milisegundos
+let jogo = setInterval(iniciarJogo, 100);
+
+
+
 
